c131d1a9ad2eba28b6f46a665060c4e42500b427
leandromancini <leomancini1990@gmail.com>
2024-11-30 20:40:32 +0000

    feat: se agrega log

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
+		fmt.Printf("Request recibida correctamente")
 		fmt.Fprint(w, `{"message": "pong"}`)
 	})
 
